cad1e67acb889bdf6474b349041693b9bc1e308b
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2024-11-15 11:14:38 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/course/calculation.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.course;
+
+/**
+ *
+ * @author Basmala
+ */
+public class calculation {
+    private long n;
+    private double d;
+    
+    public void setLongN(long n){
+        this.n=n;
+    }
+    
+    public long getLongN(){
+        return n;
+    }
+    
+    public void setDoubleN(double d){
+        this.d=d;
+    }
+    
+    public double getDoubleN(){
+        return d;
+    }
+}
