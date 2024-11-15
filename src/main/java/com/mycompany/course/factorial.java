68a26c3141fd56a5beb185c66eb1601492cb5cc1
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2024-11-15 11:13:12 +0200

    Update factorial.java

--- src/main/java/com/mycompany/course/factorial.java
+++ src/main/java/com/mycompany/course/factorial.java
@@ -8,12 +8,33 @@ package com.mycompany.course;
  *
  * @author Basmala
  */
-public class factorial {
+public class factorial extends calculation{
+    
+    public factorial(long n){
+        setLongN(n);
+    }
+    
+    public factorial(double n){
+        setDoubleN(n);
+    }
+    
     public long calculateFactorial(long n){
+        System.out.print("Long factoral is :");
         if(n<0)
             return -1;
         long ans=1;
         for(int i=1;i<=n;i++)
+            ans*=i;
+        
+        return ans;
+    }
+    
+    public double calculateFactorial(double n){
+        System.out.print("Double factoral is : ");
+        if(n<0)
+            return -1;
+        double ans=1;
+        for(double i=1;i<=n;i++)
             ans*=i;
         
         return ans;
